253267c4a4f4d4ac10686b891d3b0643dc6fb537
Leon Varga <leon.varga.hdh@gmail.com>
2023-10-13 15:38:08 +0200

    simplify user interface

--- shopping_list.py
+++ shopping_list.py
@@ -23,12 +23,21 @@ def find_item_section(item_simplified: str, db):
             return item['section']
     return None
 
-def ask_user_decision(question, options= ['y', 'n']):
+def ask_user_decision(question, options= ['y', 'n'], allow_number=False):
     decision = None
+    is_valid = False
 
-    while decision not in options:
+    while not is_valid:
         decision = input(question + "\n")
-
+        if decision is not None:
+            if decision in options:
+                is_valid = True
+                continue
+            if allow_number and decision.isnumeric():
+                is_valid = True
+                continue
+        else:
+            is_valid = False
     return decision
 
 def ask_user_for_section(db):
@@ -49,6 +58,18 @@ def ask_user_for_section(db):
         selection = input(question)
     return selection
 
+def print_sections(db):
+    def _get_section_str(section_id, section):
+        return f"[{section_id}]: {section}"
+
+    nl = '\n'
+
+    question = f"These are the available sections: \n {nl.join([_get_section_str(section_id, section) for section_id, section in enumerate(db['sections'])])}\n"
+    question += "In which section does this item belong? (Please provide the id of the section or add a new with 'a')\n" 
+    print(question)
+
+
+
 def ask_user_for_section_order(db):
     def _get_section_str(section_id, section):
         return f"[{section_id}]: {section}"
@@ -65,6 +86,44 @@ def ask_user_for_section_order(db):
         order = input(question)
 
     return [int(o) for o in order.split(' ') if len(o) > 0]
+
+
+def handle_item(item, db):
+    item_simplified = standarize_name(item)
+    item_section = None
+    print(f"'{item}'")
+    print(f"# {item} not found in item db")
+    print_sections(db)
+    ui_add_new = ask_user_decision("? Add a new item? (#/n/e/s)", ['s', 'n', 'e'], allow_number=True)
+
+    if ui_add_new == 'e':
+        item_simplified = standarize_name(input("Please provide the desired item name:"))
+        item_section = find_item_section(item_simplified, db)
+    elif ui_add_new == 's':
+        ui_section_name = input("? Provide a section name: ")
+        db['sections'].append(ui_section_name)
+        item_section = len(db['sections']) - 1 
+
+        opt.override_section_order = True
+        if len(db['section_order']) > 0:
+            db['section_order_old'] = db['section_order']
+            db['section_order'] = []
+    elif ui_add_new.isnumeric():
+        item_section = ui_add_new
+    else:
+        print(f"! Ignore {item} for sorted shopping list")
+        return False
+
+    if item_section is not None:
+        db['items'].append(
+            {
+                'name': item_simplified,
+                'section': int(item_section)
+            })
+        save_db(db)
+
+    return item_section
+
 
 if __name__ == '__main__': 
     parser = argparse.ArgumentParser("Sort your shopping list by the sections of the supermarket")
@@ -105,37 +164,9 @@ if __name__ == '__main__':
             item_section = find_item_section(item_simplified, db)
 
             while item_section is None:
-                print(f"'{item}'")
-                print(f"# {item} not found in item db")
-                ui_add_new = ask_user_decision("? Add a new item? (y/n/e)", ['y', 'n', 'e'])
-
-                if ui_add_new == 'e':
-                    item_simplified = standarize_name(input("Please provide the desired item name:"))
-                    item_section = find_item_section(item_simplified, db)
-                elif ui_add_new == 'y':
-                    ui_section = ask_user_for_section(db)
-                    
-                    if ui_section == 'a':
-                        ui_section_name = input("? Provide a section name: ")
-                        db['sections'].append(ui_section_name)
-                        ui_section = len(db['sections']) - 1 
-
-                        db['items'].append(
-                            {
-                                'name': item_simplified,
-                                'section': int(ui_section)
-                            })
-
-                        opt.override_section_order = True
-                        if len(db['section_order']) > 0:
-                            db['section_order_old'] = db['section_order']
-                            db['section_order'] = []
-                    save_db(db)
-
-                    item_section = ui_section
-
-                else:
-                    print(f"! Ignore {item} for sorted shopping list")
+                item_section = handle_item(item, db)
+                if item_section == False:
+                    item_section = None
                     break
 
             item_section_map.append({'item': item, 'section': item_section})
